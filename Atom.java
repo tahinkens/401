85e71ad8b3570d37a2fb37e9bc11df4b442aed44
tahinkens <37641110+tahinkens@users.noreply.github.com>
2018-04-14 23:59:42 -0500

    Add files via upload
    Changed marsagliaRandomGenerator() to return a single double instead of an array

--- Atom.java
+++ Atom.java
@@ -45,20 +45,28 @@ public class Atom {
      * 
      * @param timestep the amount of time passed since last update (this should
      * generally be the universal timestep of the simulation)
+     * @param neighbors an array of Atoms containing the atoms nearby to this
      */
-    public void update(double timestep) {
+    public void update(double timestep, Atom[] neighbors) {
+
+        //update potential energy and differentiate to obtain force
+        for(int i = 0; i < neighbors.length; i++) {
+            if(this.getDistanceFromNeighbor(neighbors[i]) < 5e-10) //only calculate potential from atoms that are <5 Ang away
+            pe = lennardJones(this.atomType.homopotentialWellDepth,this.getDistanceFromNeighbor(neighbors[i]),this.atomType.epsilon);
+            //does this only work for one atom at a time? Might need to be a sum of all pe's
+        }
         
+        //velocity vector needs to change due to particle interactions
         v = Math.sqrt(velocity[0]*velocity[0] + velocity[1]*velocity[1] + velocity[2]*velocity[2]);
-        p = m * v;
         ke = 0.5 * m * v*v;
-        //pe = lennard jones?
+        
         for(int i = 0; i < 3; i++) {
             momentum[i] = m * velocity[i];
         }
+        p = m * v;
         for(int i = 0; i < 3; i++) { //x1 = x0 + vt
             position[i] = position[i] + velocity[i] * timestep;
         }
-        //velocity vector needs to change due to particle interactions
     }
     
     /**
@@ -68,7 +76,8 @@ public class Atom {
      * factor of sqrt(kT/m) where k is Boltzmann's constant, T is thermodynamic 
      * temp, and m is the mass of the atom of a given species.
      * 
-     * @param dimensions How many dimensions does this particle exist in?
+     * @param dimensions How many dimensions does this particle exist in? For
+     * the extent of this project, this should be fixed at 3.
      * @param T Thermodynamic temperature (temperature)
      * @param atomType The type of atom being simulated (AtomInfo)
      * @return a randomly generated particle velocity
@@ -79,31 +88,93 @@ public class Atom {
         final double a = Math.sqrt((K_B * T) / atomType.m);
         
         for(int i = 0; i < dimensions; i++) {
-            velocity[i] = a * marsagliaRandomGenerator()[0];
+            velocity[i] = a * marsagliaRandomGenerator();
         }
         return velocity;
     }
     
     /**
+     * Calculates the potential between two atoms with a potential well depth e,
+     * interatomic separation of s, and equilibrium separation of r.
+     * 
+     * @param e Potential well depth (energy)
+     * @param s Interatomic separation (length)
+     * @param r Equilibrium separation (length)
+     * @return Interatomic potential (energy)
+     */
+    private double lennardJones(double e, double s, double r) {
+
+        return 4 * e * (Math.pow((s/r),12) - Math.pow((s/r),6));
+    }
+    
+    /**
      * Generates a pair of normally distributed random variables with mean 0 and
      * variance 1. Used for assigning randomly generated velocities to 
-     * particles.
+     * particles. The Marsaglia method generates two paired values by default,
+     * but since only one is needed only one is returned.
      * 
-     * @return an array containing two randomly generated numbers
+     * @return a random, normally distributed double between -1 and 1
      */
-    private double[] marsagliaRandomGenerator() {
+    private double marsagliaRandomGenerator() {
         
-        double x = 0, y = 0, s = 2; //s must be initialized to something > 1
-        double[] rv = new double[2];
+        double x = 0, y, s = 2; //s must be initialized to something > 1
+        //double[] rv = new double[2];
+        double rv;
         
         while(s > 1) {
             x = rng.nextDouble() * 2 - 1; //uniform double b/w -1 and 1
             y = rng.nextDouble() * 2 - 1;
             s = x*x + y*y;
         }
-        rv[0] = x * Math.sqrt((-2 * Math.log(s)) / s); //transform to normals
-        rv[1] = y * Math.sqrt((-2 * Math.log(s)) / s);
+        //rv[0] = x * Math.sqrt((-2 * Math.log(s)) / s); //transform to normals
+        //rv[1] = y * Math.sqrt((-2 * Math.log(s)) / s);
+        rv = x * Math.sqrt((-2 * Math.log(s)) / s);
         return rv;
+    }
+    
+    /**
+     * Generates a random particle speed based on the Maxwell-Boltzmann 
+     * distribution based on thermodynamic temperature and particle mass. Speed
+     * given in m/s.
+     * 
+     * @param T Thermodynamic temperature (temperature)
+     * @param atomType The type of atom being simulated (AtomInfo)
+     * @return a randomly generated particle speed in m/s
+     * 
+     * @deprecated This function isn't used and instead initial particle 
+     * velocities are determined using a Marsaglia random generator and the 
+     * initializeVelocityVector() function above. It's being left in for
+     * possible future use and posterity.
+     */
+    @Deprecated
+    private double maxwellBoltzmann(double T, AtomInfo atomType) {
+       
+        final double a = Math.sqrt((K_B * T) / atomType.m); //where K_B is Boltzmann's constant and m is particle mass
+        final double x = rng.nextDouble();
+        
+        return Math.sqrt(2/PI) * ((x*x * Math.exp(-(x*x) / (2 * a*a))) / (a*a*a)); //FIXME this is the y-value of the pdf not speed
+    }
+    
+    /**
+     * 
+     * @param neighbor the neighboring Atom
+     * @return the distance between this Atom and the neighboring Atom
+     */
+    public double getDistanceFromNeighbor(Atom neighbor) {
+        
+        double d; //distance
+        double[] distance = new double[3]; //difference vector between positions
+        
+        for(int i = 0; i < 3; i++) { //why 3?
+            distance[i] = this.position[i] - neighbor.position[i];
+        }
+        d = Math.sqrt(distance[0]*distance[0] + distance[1]*distance[1] + distance[2]*distance[2]);
+        return d;
+    }
+    
+    public double[] getPosition() {
+        
+        return position;
     }
     
     /**
